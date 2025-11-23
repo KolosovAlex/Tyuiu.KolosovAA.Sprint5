8ad1fc78ff67d6618f895da979ad42093e031680
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-11-24 03:38:31 +0500

    Task2

---
+++ Tyuiu.KolosovAA.Sprint5.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KolosovAA.Sprint5.Task2.V8.Lib;
+
+
+namespace Tyuiu.KolosovAA.Sprint5.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\sasha\source\repos\Tyuiu.KolosovAA.Sprint5\Tyuiu.KolosovAA.Sprint5.Task2.V8\bin\Debug\OutPutFileTask2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            Assert.IsTrue(fileInfo.Exists);
+        }
+    }
+}
