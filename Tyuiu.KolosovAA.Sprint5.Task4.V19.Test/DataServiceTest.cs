9ddcf019ba1eca97485a5d2bd2b7535bd3002412
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-11-24 04:11:14 +0500

    task4

---
+++ Tyuiu.KolosovAA.Sprint5.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.KolosovAA.Sprint5.Task4.V19.Lib;
+namespace Tyuiu.KolosovAA.Sprint5.Task4.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
