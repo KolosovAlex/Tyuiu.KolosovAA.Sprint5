136245db9619955adf277c73fb6f4db5e2b2678a
Alexandr Kolosov <Tyuiu.SpirinISKolosovaA@outlook.com>
2025-11-24 05:04:49 +0500

    Task6

---
+++ Tyuiu.KolosovAA.Sprint5.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KolosovAA.Sprint5.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KolosovAA.Sprint5.Task6.V28/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KolosovAA.Sprint5.Task6.V28.Lib;
+namespace Tyuiu.KolosovAA.Sprint5.Task6.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
